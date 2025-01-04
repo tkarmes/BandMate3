846bab1119bddb33e55a71c39f949e6e09e495e9
Travis <tkarmes@gmail.com>
2025-01-03 23:40:07 -0600

    touch up

--- src/main/java/com/techelevator/controller/AuthenticationController.java
+++ src/main/java/com/techelevator/controller/AuthenticationController.java
@@ -3,6 +3,8 @@ package com.techelevator.controller;
 import javax.validation.Valid;
 
 import com.techelevator.exception.DaoException;
+import com.techelevator.exception.UserNotFoundException;
+import com.techelevator.exception.UserDeletionException;
 import com.techelevator.model.*;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -25,7 +27,7 @@ public class AuthenticationController {
 
     private final TokenProvider tokenProvider;
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
-    private UserDao userDao;
+    private final UserDao userDao;
 
     public AuthenticationController(TokenProvider tokenProvider, AuthenticationManagerBuilder authenticationManagerBuilder, UserDao userDao) {
         this.tokenProvider = tokenProvider;
@@ -70,5 +72,20 @@ public class AuthenticationController {
         }
     }
 
-}
+    @DeleteMapping("/users/{username}")
+    public ResponseEntity<Void> deleteUserByUsername(@PathVariable String username) {
+        try {
+            // Assuming the user's ID can be retrieved from the security context
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            Long actingUserId = ((User) authentication.getPrincipal()).getUserId(); // Assuming User extends UserDetails
 
+            userDao.deleteUserByUsername(username, actingUserId);
+            return ResponseEntity.noContent().build(); // 204 No Content if successful
+        } catch (UserNotFoundException e) {
+            return ResponseEntity.notFound().build(); // 404 Not Found if user not found
+        } catch (UserDeletionException e) {
+            // You might want to return a more specific status code or message here
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build(); // 403 Forbidden if the user doesn't have permission
+        }
+    }
+}

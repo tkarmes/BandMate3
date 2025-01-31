ea597695642d7a13fad1ad8bed82d10cef22189b
Travis <tkarmes@gmail.com>
2025-01-31 13:52:14 -0600

    fixed creat.sh and can now see users until I change tables on schema_1 again

--- database/create.sh
+++ database/create.sh
@@ -4,6 +4,5 @@ BASEDIR=$(dirname $0)
 DATABASE=bandmate3
 psql -U postgres -f "$BASEDIR/dropdb.sql" &&
 createdb -U postgres $DATABASE &&
-psql -U postgres -d $DATABASE -f "$BASEDIR/schema.sql" &&
-psql -U postgres -d $DATABASE -f "$BASEDIR/data.sql" &&
+psql -U postgres -d $DATABASE -f "$BASEDIR/schema_1.sql" &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/user.sql"
